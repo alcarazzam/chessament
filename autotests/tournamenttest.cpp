7c63f844cf0ff5bb8590673329c9d134e50b2c34
Manuel Alcaraz Zambrano <manuelalcarazzam@gmail.com>
2024-11-23 18:22:27 +0100

    Add tournament tests

--- autotests/tournamenttest.cpp
+++ autotests/tournamenttest.cpp
@@ -14,12 +14,39 @@ class TournamentTest : public QObject
 private Q_SLOTS:
 
     void testNewTournament();
+    void testToJson();
+    void testTrf();
 };
 
 void TournamentTest::testNewTournament()
 {
     auto t = new Tournament();
     QCOMPARE(t->name(), QStringLiteral(""));
+}
+
+void TournamentTest::testToJson()
+{
+    auto t = new Tournament();
+    t->setName(QStringLiteral("Test tournament"));
+
+    auto json = t->toJson();
+
+    QCOMPARE(json[QStringLiteral("tournament")].toObject()[QStringLiteral("name")], QStringLiteral("Test tournament"));
+}
+
+void TournamentTest::testTrf()
+{
+    auto t = new Tournament();
+    t->setName(QStringLiteral("Test tournament"));
+
+    auto trf = t->toTrf();
+
+    QVERIFY(trf.contains(QStringLiteral("012 Test tournament")));
+
+    t = new Tournament();
+    auto ok = t->readTrf(QTextStream(&trf));
+
+    QVERIFY(ok.has_value() && ok);
 }
 
 QTEST_GUILESS_MAIN(TournamentTest)
